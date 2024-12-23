8af8777a624e4269117ce6f4acbdf8c1e3f1b8b1
Claudius Sachinda <claudius@bluegrassdigital.com>
2024-12-23 20:41:21 +0200

    menus and contact us page blocks

--- wp-content/themes/livan/functions.php
+++ wp-content/themes/livan/functions.php
@@ -51,6 +51,7 @@ function livan_setup()
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__('Primary', 'livan'),
+			'menu-2' => esc_html__('Footer', 'livan'),
 		)
 	);
 
--- wp-content/themes/livan/page.php
+++ wp-content/themes/livan/page.php
@@ -19,7 +19,7 @@ get_header();
 	<?php
 	// Check if the 'has_breadcrumb' field is true
 	if (get_field('has_breadcrumb')): ?>
-		<section class="breadcrumb"
+		<section class="breadcrumb curve-bottom"
 			style="background-image: url('<?php echo esc_url(get_template_directory_uri() . '/images/breadcrumbs.jpg'); ?>');">
 			<div class="container">
 				<h1 class="breadcrumb__title"><?php the_title(); ?></h1>
